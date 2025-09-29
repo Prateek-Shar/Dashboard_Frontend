033a7e22475e06ff27aebe9143e637c4f44893a7
Prateek <rahuls12153@gmail.com>
2025-09-29 19:08:34 +0530

    Final Build

--- src/Pages/Customer.tsx
+++ src/Pages/Customer.tsx
@@ -2,15 +2,12 @@ import Table_content from "../components/Customer_com/Customer_table"
 import Stats from "../components/Customer_com/Customer_stats";
 import Customer_head from "../components/Customer_com/customer_head";
 import Add_customer from "../components/Customer_com/Add_customer_bt"
-import check from "../images/check.png"
-import cross from "../images/cross.png"
-import { useAlert } from "../context/result";
 import { StatsProvider } from "../context/customers_stats_context";
 
 
 const Customer_page = () => {
 
-    const { successVisible, failureVisible } = useAlert();
+
 
     return (
         <div className="bg-[#edede9] w-full relative">
@@ -21,6 +18,7 @@ const Customer_page = () => {
 
             <StatsProvider>
                 <div className="w-full flex justify-evenly items-center mt-10">
+
                     <div className="w-[65%] flex items-center justify-center">
                         <Stats />
                     </div>
@@ -36,32 +34,6 @@ const Customer_page = () => {
                     <Table_content />
                 </div>  
             </StatsProvider>
-
-
-            {successVisible && (
-                <div className="w-[25%] hidden bg-amber-600 shadow-2xl absolute bottom-0 right-0">
-                    <div className="">
-                        <img src={check} />
-                    </div>
-
-                    <div className="">
-                        <p>Data Entered Successfully</p>
-                    </div>
-                </div>
-            )}
-
-
-            {failureVisible && (
-                <div className="w-[25%] hidden bg-blue-600 shadow-2xl absolute bottom-0 right-0">
-                    <div className="">
-                        <img src={cross} />
-                    </div>
-
-                    <div className="">
-                        <p>Unable to send data</p>
-                    </div>
-                </div>
-            )}
 
         </div>
     )
